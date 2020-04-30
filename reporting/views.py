ad25bd4d1f54461fb3c4bad5d7acd03c3c458022
Evgeny Levenko <levenko.evgeny@yandex.by>
2020-04-30 10:21:48 +0300

    fix db

--- reporting/views.py
+++ reporting/views.py
@@ -9,9 +9,8 @@ def employee(request):
     author_list = Author.objects.all()
     if 'employee_id' in request.GET:
         author = get_object_or_404(Author, pk=request.GET['employee_id'])
-        publicationlist = author.sciencework_set.all().filter(year__gte=request.GET['year_since']).filter(
+        publication_list = author.sciencework_set.all().filter(year__gte=request.GET['year_since']).filter(
             year__lte=request.GET['year_till']).order_by('year', 'kind__publicationkind')
-
 
         nir_list = NIR.objects.filter(start_date__year__lte=request.GET['year_till']).exclude(end_date__year__lt=request.GET['year_since'])
         # filter(start_date__year__lte=request.GET['year_till']).exclude(end_date__year__lt=request.GET['year_since'])
@@ -43,7 +42,7 @@ def employee(request):
             year_list.append(str(year_since))
             year_since = year_since + 1
         return render(request, 'reporting/employee.html', {'author': author,
-                                                           'publicationlist': publicationlist,
+                                                           'publicationlist': publication_list,
                                                            'd': d,
                                                            'suf': suf,
                                                            'year_list': year_list,
