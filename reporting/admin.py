0ab5a6e889a272a7a305c44d6ad4a2656c784160
Evgeny Levenko <levenko.evgeny@yandex.by>
2020-05-04 12:12:31 +0300

    add criterion value

--- reporting/admin.py
+++ reporting/admin.py
@@ -2,6 +2,19 @@ from django.contrib import admin
 from .models import *
 
 
-admin.site.register(RatingEmployee)
-admin.site.register(RatingTableEmployee)
-admin.site.register(RatingTableSubdivision)
+@admin.register(RatingEmployee)
+class RatingEmployeePageAdmin(admin.ModelAdmin):
+    list_display = ('id','criterion', 'value')
+    search_fields = ['criterion']
+
+
+@admin.register(RatingTableEmployee)
+class RatingEmployeePageAdmin(admin.ModelAdmin):
+    list_display = ('id','author', 'rating', 'place')
+    search_fields = ['author']
+
+
+@admin.register(RatingTableSubdivision)
+class RatingSubdivisionPageAdmin(admin.ModelAdmin):
+    list_display = ('id','subdivision', 'rating', 'place')
+    search_fields = ['subdivision']
